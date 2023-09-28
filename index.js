6649c36fd8159490638628a058bb2f4106fef86e
Wiztech-01 <lynnnyaoke@gmail.com>
2023-09-28 05:23:15 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let p = document.getElementById('text');
+    document.querySelector("p").textContent = "This is really cool!"
+})
